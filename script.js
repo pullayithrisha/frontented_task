fc1aa036875b8065701813f5a98f0c69b9ecbd4f
pullayithrisha <pullayithrisha@gmail.com>
2024-12-24 08:40:18 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('menu-icon').addEventListener('click', () => {
+    alert('Menu toggled!');
+  });
+  
+  document.getElementById('notification-icon').addEventListener('click', () => {
+    alert('Notifications clicked!');
+  });
+  
